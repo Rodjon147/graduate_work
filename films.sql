7accb0088613f8c312ed24e211b27143419e59b6
Rodjon <rshipko147@gmail.com>
2022-06-21 23:59:23 +0800

    add big update4

--- films.sql
+++ films.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Май 25 2022 г., 22:16
+-- Время создания: Июн 21 2022 г., 17:43
 -- Версия сервера: 10.3.16-MariaDB
 -- Версия PHP: 7.3.6
 
@@ -25,14 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `array`
+-- Структура таблицы `arrayfilms`
 --
 
-CREATE TABLE `array` (
+CREATE TABLE `arrayfilms` (
   `id` int(11) NOT NULL,
   `id_film` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `id_collection` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `arrayfilms`
+--
+
+INSERT INTO `arrayfilms` (`id`, `id_film`, `id_collection`) VALUES
+(66, 29, 19),
+(67, 30, 19),
+(68, 32, 19),
+(69, 24, 20),
+(70, 26, 20),
+(71, 31, 20),
+(72, 33, 20),
+(73, 32, 20),
+(74, 30, 20),
+(75, 29, 20);
 
 -- --------------------------------------------------------
 
@@ -43,9 +59,16 @@ CREATE TABLE `array` (
 CREATE TABLE `collections` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `description` varchar(255) NOT NULL,
-  `id_array` int(11) NOT NULL
+  `description` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `collections`
+--
+
+INSERT INTO `collections` (`id`, `name`, `description`) VALUES
+(19, 'Первая подборка', 'Это первая подборка'),
+(20, 'Вторая подборка', 'Это новая подборка');
 
 -- --------------------------------------------------------
 
@@ -65,10 +88,11 @@ CREATE TABLE `estimation` (
 --
 
 INSERT INTO `estimation` (`id`, `estimation`, `id_film`, `id_user`) VALUES
-(32, 9, 24, 5),
-(40, 7, 23, 5),
-(41, 9, 21, 5),
-(42, 7, 22, 5);
+(89, 8, 24, 7),
+(90, 10, 29, 7),
+(96, 10, 29, 1),
+(97, 9, 24, 1),
+(98, 9, 31, 1);
 
 -- --------------------------------------------------------
 
@@ -90,7 +114,7 @@ CREATE TABLE `feedback` (
 
 INSERT INTO `feedback` (`id`, `feedback`, `id_film`, `id_user`, `date`) VALUES
 (12, 'asdas', 24, 1, '2022-05-26 03:44:08'),
-(16, 'qweqwe', 24, 5, '2022-05-26 04:08:21');
+(26, 'Отличный фильм', 29, 7, '2022-06-21 09:21:18');
 
 -- --------------------------------------------------------
 
@@ -101,7 +125,7 @@ INSERT INTO `feedback` (`id`, `feedback`, `id_film`, `id_user`, `date`) VALUES
 CREATE TABLE `films` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` text NOT NULL,
   `genre` varchar(100) NOT NULL,
   `director` varchar(100) NOT NULL,
   `year` year(4) NOT NULL,
@@ -116,10 +140,13 @@ CREATE TABLE `films` (
 --
 
 INSERT INTO `films` (`id`, `name`, `description`, `genre`, `director`, `year`, `country`, `cover`, `estimation`, `countUsers`) VALUES
-(21, 'Крестный отец', 'В семье крупного нью-йоркского мафиози наметился кризис. Революция в гангстерском кино и начало большого эпоса', 'Гангстерский фильм', ' Фрэнсис Форд Коппола', 1972, ' США', 'public\\images\\1653342456160-Крестный отец.webp', 9, 1),
-(22, 'Крестный отец 2', 'Юность Вито Корлеоне и первые шаги его сына Майкла в роли главы клана — сразу и приквел, и сиквел. Шесть «Оскаров»', 'Гангстерский фильм', ' Фрэнсис Форд Коппола', 1974, ' США', 'public\\images\\1653418393194-Крестный отец2.webp', 7, 1),
-(23, 'Крестный отец 3', 'Майкл Корлеоне мечтает искупить грехи и сделать свой бизнес легальным, но прошлое не отпускает. Финал трилогии', 'Гангстерский фильм', ' Фрэнсис Форд Коппола', 1990, ' США', 'public\\images\\1653418540041-Крестный отец3.webp', 7, 1),
-(24, 'Человек-паук: Нет пути домой', 'Жизнь и репутация Питера Паркера оказываются под угрозой, поскольку Мистерио раскрыл всему миру тайну личности Человека-паука. Пытаясь исправить ситуацию, Питер обращается за помощью к Стивену Стрэнджу, но вскоре всё становится намного опаснее.', 'Фантастический фильм', ' Джон Уоттс', 2021, ' США', 'public\\images\\1653509457917-Человек-паук.webp', 9, 1);
+(24, 'Человек-паук: Нет пути домой', 'Жизнь и репутация Питера Паркера оказываются под угрозой, поскольку Мистерио раскрыл всему миру тайну личности Человека-паука. Пытаясь исправить ситуацию, Питер обращается за помощью к Стивену Стрэнджу, но вскоре всё становится намного опаснее.', 'Фантастический фильм', ' Джон Уоттс', 2021, ' США', 'public\\images\\1653509457917-Человек-паук.webp', 8.5, 2),
+(26, 'Крестный отец', 'Фильм про ганстеров 1986 года', 'Боевик', 'Иван Петров', 1986, 'США', 'public\\images\\1654520034496-Крестный отец3.webp', 0, 0),
+(29, 'Драйв', 'Великолепный водитель – при свете дня он выполняет каскадерские трюки на съёмочных площадках Голливуда, а по ночам ведет рискованную игру. Но один опасный контракт – и за его жизнь назначена награда. Теперь, чтобы остаться в живых и спасти свою очаровательную соседку, он должен делать то, что умеет лучше всего – виртуозно уходить от погони.', 'Боевик', ' Николас Виндинг Рефн', 2011, 'США', 'public\\images\\1655754370917-Драйв.webp', 10, 2),
+(30, 'Бегущий по лезвию 2049', 'В недалеком будущем мир населен людьми и репликантами, созданными выполнять самую тяжелую работу. Работа офицера полиции Кей - держать репликантов под контролем в условиях нарастающего напряжения... Пока он случайно не становится обладателем секретной информации, которая ставит под угрозу существование всего человечества. Желая найти ключ к разгадке, Кей решает разыскать Рика Декарда, бывшего офицера специального подразделения полиции Лос-Анджелеса, который бесследно исчез много лет назад.', 'Боевик', ' Дени Вильнёв', 2017, ' США', 'public\\images\\1655754463652-Бегущий по лезвию.webp', 0, 0),
+(31, 'Великий Гэтсби', 'Весной 1922 года, в эпоху разлагающейся морали, блистательного джаза и «королей контрабандного алкоголя», Ник Каррауэй приезжает из Среднего Запада в Нью-Йорк. Преследуя собственную американскую мечту, он селится по соседству с таинственным, известным своими вечеринками миллионером Джеем Гэтсби, а на противоположном берегу бухты проживают его кузина Дэйзи и её муж, повеса и аристократ, Том Бьюкенен. Так Ник оказывается вовлечённым в захватывающий мир богатых — их иллюзий, любви и обманов. Он становится свидетелем происходящего в этом мире и пишет историю невозможной любви, вечных мечтаний и человеческой трагедии, которые являются отражением современных времен и нравов.', 'Мелодрама', ' Баз Лурман', 2013, 'США', 'public\\images\\1655754642185-Великий Гэтсби.webp', 9, 1),
+(32, 'Интерстеллар', 'Когда засуха, пыльные бури и вымирание растений приводят человечество к продовольственному кризису, коллектив исследователей и учёных отправляется сквозь червоточину (которая предположительно соединяет области пространства-времени через большое расстояние) в путешествие, чтобы превзойти прежние ограничения для космических путешествий человека и найти планету с подходящими для человечества условиями.', 'Фантастический фильм', ' Кристофер Нолан', 2014, ' США', 'public\\images\\1655754748492-Интерстеллар.webp', 0, 0),
+(33, 'Гладиатор', 'В великой Римской империи не было военачальника, равного генералу Максимусу. Непобедимые легионы, которыми командовал этот благородный воин, боготворили его и могли последовать за ним даже в ад. Но случилось так, что отважный Максимус, готовый сразиться с любым противником в честном бою, оказался бессилен против вероломных придворных интриг. Генерала предали и приговорили к смерти. Чудом избежав гибели, Максимус становится гладиатором.', 'Боевик', ' Ридли Скотт', 2000, ' США', 'public\\images\\1655754863021-Гладиатор.webp', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -141,18 +168,20 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `role`, `avatar`) VALUES
-(1, 'Rodjon', 'rshipko147@gmail.com', '$2b$07$hHUMKkdI3i9XlMog9ekxDO928RY1cNX.ltorCrQesQv8lVzB/By4G', 'admin', 'null'),
-(5, 'nepenf', 'nepenf@mail.com', '$2b$07$2fx6CZqF6gisx9f5nZckuu0aRsel2NCqW7sibl4iiQKw3kc9zhbY6', 'user', 'null');
+(1, 'Rodjon', 'rshipko147@gmail.com', '$2b$07$hHUMKkdI3i9XlMog9ekxDO928RY1cNX.ltorCrQesQv8lVzB/By4G', 'admin', 'public\\avatar\\1655766528759-photo_2022-04-19_22-22-36.jpg'),
+(7, 'qwerty', 'qwerty@gmail.com', '$2b$07$nuru1VEt/j19Exmlb3axRuv/Iv/QNDLHXDWtS1f9itFBJ7c0h3Sa.', 'user', 'null');
 
 --
 -- Индексы сохранённых таблиц
 --
 
 --
--- Индексы таблицы `array`
+-- Индексы таблицы `arrayfilms`
 --
-ALTER TABLE `array`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `arrayfilms`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_film` (`id_film`),
+  ADD KEY `array_ibfk_2` (`id_collection`);
 
 --
 -- Индексы таблицы `collections`
@@ -164,7 +193,9 @@ ALTER TABLE `collections`
 -- Индексы таблицы `estimation`
 --
 ALTER TABLE `estimation`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_film` (`id_film`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Индексы таблицы `feedback`
@@ -191,44 +222,58 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT для таблицы `array`
+-- AUTO_INCREMENT для таблицы `arrayfilms`
 --
-ALTER TABLE `array`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `arrayfilms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
 
 --
 -- AUTO_INCREMENT для таблицы `collections`
 --
 ALTER TABLE `collections`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT для таблицы `estimation`
 --
 ALTER TABLE `estimation`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=99;
 
 --
 -- AUTO_INCREMENT для таблицы `feedback`
 --
 ALTER TABLE `feedback`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT для таблицы `films`
 --
 ALTER TABLE `films`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `arrayfilms`
+--
+ALTER TABLE `arrayfilms`
+  ADD CONSTRAINT `arrayfilms_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `films` (`id`),
+  ADD CONSTRAINT `arrayfilms_ibfk_2` FOREIGN KEY (`id_collection`) REFERENCES `collections` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `estimation`
+--
+ALTER TABLE `estimation`
+  ADD CONSTRAINT `estimation_ibfk_1` FOREIGN KEY (`id_film`) REFERENCES `films` (`id`),
+  ADD CONSTRAINT `estimation_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`);
 
 --
 -- Ограничения внешнего ключа таблицы `feedback`
